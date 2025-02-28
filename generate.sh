c8c4317b5b8203959a3f8d604d045ceb5aee6f85
GhidraDragon <bo@shangsoftware.ru>
2025-02-28 09:12:56 -0500

    exhibits needs improvement

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 python3.12 tflegal.py --firm_name "PDFSage Inc." \
-  --case "Washington State Superior Court, King County" \
+  --case "Commonwealth of MA Superior Court - Middlesex County" \
   --file lawsuit.txt
