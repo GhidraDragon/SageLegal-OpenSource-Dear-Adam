bd0f870381979721327322acb2e5b7c80d501a20
GhidraDragon <bo@shangsoftware.ru>
2025-03-01 09:27:57 -0500

    pokimane

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-python3.12 tflegal.py --firm_name "PDFSage Inc." \
-  --case "Commonwealth of MA Superior Court - Middlesex County" \
+python3.12 tflegal.py --firm_name "PDFSage Inc. makers of Artificial General Intelligence" \
+  --case "CA Superior Court of Santa Clara County" \
   --file lawsuit.txt
