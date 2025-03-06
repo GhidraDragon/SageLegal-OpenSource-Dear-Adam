94a0a4c859335843806c6f87aed4bcc1b22761de
GhidraDragon <bo@shangsoftware.ru>
2025-03-06 09:41:58 -0500

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 python3.12 tflegal.py --firm_name "PDFSage Inc." \
-  --case "MA Superior Court Middlsex County" \
+  --case "CA Superior Court at San Francisco County" \
   --file lawsuit.txt \
   --exhibits poki1.jpg poki2.jpg
