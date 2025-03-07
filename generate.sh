e1a497da398814da6dc52ced767bd69d44317468
GhidraDragon <bo@shangsoftware.ru>
2025-03-07 04:36:16 -0500

    h1b

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 python3.12 tflegal.py --firm_name "PDFSage Inc." \
   --case "CA Superior Court at San Francisco County" \
   --file lawsuit.txt \
-  --exhibits poki1.jpg poki2.jpg poki3.jpg
+  --exhibits reck1.jpg reck2.jpg reck3.jpg reck4.jpg reck5.jpg reck6.jpg
