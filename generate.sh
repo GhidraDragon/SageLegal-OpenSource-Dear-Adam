0dad107dd38fc4da8e9b4764fd31ec7a062de251
Bo Shang <bo@shangsoftware.ru>
2025-03-08 01:25:08 -0500

    s

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 python3.11 tflegal.py --firm_name "PDFSage Inc." \
-  --case "CA Superior Santa Clara County - Bo Shang vs NVIDIA and Microsoft" \
+  --case "CA Superior County of SF - Bo Shang vs Twitch and Amazon" \
   --file lawsuit.txt \
-  --exhibits azure1.jpg azure2.jpg azure3.jpg azure4.jpg azure5.jpg azure6.jpg azure7.jpg azure8.jpg
+  --exhibits reck1.jpg reck2.jpg reck3.jpg reck4.jpg reck5.jpg reck6.jpg
