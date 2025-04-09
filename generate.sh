e95dc1e02be5fc2ea346142b72053e2b426cd413
Bo Shang <boshang1988@icloud.com>
2025-04-09 12:05:41 -0400

    dear adam

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-python3.11 tflegal.py --firm_name "PDFSage Inc." \
-  --case "CA Superior County of SF - Bo Shang vs Twitch and Amazon" \
-  --file lawsuit.txt \
-  --exhibits reck1.jpg reck2.jpg reck3.jpg reck4.jpg reck5.jpg reck6.jpg
+python3.12 tflegal.py --firm_name "PDFSage Inc." \
+  --case "Middlesex Superior Court: Bo Shang vs MIT and ADAM HARTZ" \
+  --file dear_adam.txt \
+  --exhibits adam1 adam2 adam3 adam4 adam5
